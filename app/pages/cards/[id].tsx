5f79063fa4ef9a0144b44e5800079d7abce9253c
大城亮 <hiltoc.katsu@gmail.com>
2024-08-08 20:03:44 +0900

    プロパティを英語表記に変更

--- app/pages/cards/[id].tsx
+++ app/pages/cards/[id].tsx
@@ -171,7 +171,7 @@ const EditTask: React.FC = () => {
           )}
         </div>
         <div style={styles.property}>
-          <span style={styles.propertyLabel}>著者</span>
+          <span style={styles.propertyLabel}>Author</span>
           {isEditingAuthor ? (
             <input
               type="text"
@@ -192,7 +192,7 @@ const EditTask: React.FC = () => {
           )}
         </div>
         <div style={styles.property}>
-          <span style={styles.propertyLabel}>カバー画像</span>
+          <span style={styles.propertyLabel}>Cover Image</span>
           <div style={styles.coverImageContainer}>
             {preview && <img src={preview} alt="カバー画像のプレビュー" style={styles.preview} />}
             <div style={styles.addFileContainer} onClick={handleAddFileClick}>
@@ -230,7 +230,7 @@ const EditTask: React.FC = () => {
           )}
         </div>
         <div style={styles.property}>
-          <span style={styles.propertyLabel}>説明</span>
+          <span style={styles.propertyLabel}>Description</span>
           {isEditingDescription ? (
             <input
               type="text"
